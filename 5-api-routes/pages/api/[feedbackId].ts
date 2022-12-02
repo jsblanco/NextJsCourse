768665bbcf05b01b9a486bd23e5b874ae102af39
Jorge Sánchez Blanco <jorgesanchezblanco1989@gmail.com>
2022-12-02 13:43:31 +0100

    Stated API project

--- 5-api-routes/pages/api/[feedbackId].ts
+++ 5-api-routes/pages/api/[feedbackId].ts
@@ -2,10 +2,8 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { getFeedbackPath, getStoredFeedbackItems } from './feedback';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    console.log(req.query)
-    const feedbackId = req.query?.feedbackId ?? '';
-    const requestedFeedback = getStoredFeedbackItems(getFeedbackPath()).find(item => +item.id === +feedbackId)
+    const feedbackId = req.query?.feedbackId;
+    const requestedFeedback = feedbackId && getStoredFeedbackItems(getFeedbackPath()).find(item => parseInt(item.id) === parseInt(feedbackId.toString()))
 
     if (requestedFeedback) return res.status(200).json({ feedback: requestedFeedback })
-
 }
